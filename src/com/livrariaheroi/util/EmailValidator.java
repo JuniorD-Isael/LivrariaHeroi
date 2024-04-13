6ff492ef69366866fd7618d27c5f5307203e9575
JuniorD-Isael <isaeldjunior@gmail.com>
2024-04-13 18:29:21 -0300

    Criada e implemtada a classe EmailValidator

--- src/com/livrariaheroi/util/EmailValidator.java
+++ src/com/livrariaheroi/util/EmailValidator.java
@@ -1,8 +1,24 @@
 package com.livrariaheroi.util;
 
+import java.util.Scanner;
+
 public class EmailValidator {
-    public static boolean isValido(String email) {
+    public static String isValido() {
+        Scanner scanner = ScannerUtil.getScanner();
+
+        String email;
+
         // O email devera ter um @ e um ponto
-        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+        while (true) {
+            System.out.println("Digite o email do usuário: ");
+            email = scanner.nextLine();
+            if (email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
+                break;
+            } else {
+                System.out.println("Email inválido");
+            }
+        }
+
+        return email;
     }
 }
---
+++ src/com/livrariaheroi/util/TelelefoneValidator.java
@@ -0,0 +1,4 @@
+package com.livrariaheroi.util;
+
+public class TelelefoneValidator {
+}
